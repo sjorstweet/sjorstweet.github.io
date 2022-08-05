ed4cb69035d6c80285a1cf4ebbff9d62409941ce
Sjors <sjorshekerman@live.nl>
2022-08-05 15:03:27 +0200

    skrrrt

--- scriptie.js
+++ scriptie.js
@@ -1,56 +1,95 @@
 let container = document.getElementById('container')
+let button = document.getElementById('button')
 
-let tweetDiv
-let tweetImage
-let tweetText
+let currentIndex = 0
 
-for(let i = 1; i < 35; i++){
-    tweetDiv = document.createElement('div')
-    tweetDiv.className = 'tweet'
+let prompts = [
+    "Flora",
+    "Fauna",
+    "Bij de dierentuin",
+    "Een Computer Term",
+    "Een voertuig",
+    "Een technologisch merk",
+    "Een elektronisch object",
+    "Een app",
+    "Iest wat je doodt als het op je valt",
+    "Iets dat je kan vervormen met je handen",
+    "Iets baap gerelateerds",
+    "Iets waar je mee kan gooien",
+    "Iets verslavends",
+    "Iets dat je op straat kan vinden",
+    "Iets dat je op school kan vinden",
+    "Drinken",
+    "Eeten",
+    "Bij de snackbar",
+    "Iets dat je in een restaurant kan bestellen",
+    "Iets in de supermarkt",
+    "Bij de slager",
+    "Bij de groente/fruit boer",
+    "Bij de bakker",
+    "Iets in de ruimte",
+    "Iets scheikundigs",
+    "Iets natuurkundigs",
+    "Iets aardrijkskundigs",
+    "Iets biologisch",
+    "Iets brandbaars",
+    "Iets in je lichaam",
+    "Iets wiskundigs",
+    "Een stad of dorp",
+    "Een land",
+    "Een water of eiland",
+    "Een niet Engels buitenlands woord",
+    "Een merk/bedrijf",
+    "Iets uit de geschiedenis",
+    "Valuta",
+    "Een god of godheid",
+    "Iets uit de mythologie",
+    "Een baan",
+    "Een sportclub",
+    "Iet sportgerelateerds",
+    "Een reeds overleden persoon",
+    "Een personage uit een boek/film",
+    "Een boek/cartoon/strip",
+    "Een film",
+    "Een personage uit een videospel",
+    "Een term uit een videospel",
+    "Een videospel",
+    "Een pokemon",
+    "Iets sprookjesachtig",
+    "Een meme",
+    "Een bekend persoon",
+    "Een serie",
+    "Een item uit een videogame",
+    "Een fictief persoon",
+    "Een evenement/attractie"
+];
 
-    tweetImage = document.createElement('img')
-    tweetImage.src = `${i}.png`
+prompts = this.shuffle(prompts)
 
-    tweetText = document.createElement('div')
-    tweetText.id = i;
-    switch(i){
-        case 4:
-            tweetText.innerHTML = 'They aint gonna catch me slipping'
-            break;
-        case 7:
-            tweetText.innerHTML = 'Eerlijk was dik spel op de iPod'
-            break;
-        case 8:
-        tweetText.innerHTML = 'Begrijp je'
-        break;
-        case 9:
-            tweetText.innerHTML = 'Enige wat ik heb onthouden is dat Ouagadougou de hoofdstad van Burkina Faso is. En ja ik heb de spelling opgezocht op google.'
-        break;
-        case 12:
-            tweetText.innerHTML = 'Ga ik nu wel steady op'
-        break;
-        case 14:
-            tweetText.innerHTML = 'Geen leugens hier'
-        break;
-        case 15:
-            tweetText.innerHTML = 'Nog steeds mood'
-        break;
-        case 18:
-            tweetText.innerHTML = 'Blijkbaar kende ik geen Engelse namen'
-        break;
-        case 23:
-            tweetText.innerHTML = 'Toen deze term nog maar 1 betekenis had'
-        break;
-        case 30:
-            tweetText.innerHTML = 'Geen idee wat hier mis ging'
-        break;
-        default:
-            tweetText.innerHTML = null
+const string = "abcdefghijklmnopqrstuvwxyz";
+this.button.innerHTML = ("Begin met de letter: " + string[Math.floor(Math.random() * string.length)].toUpperCase())
+
+function NextCategory(){
+    this.button.innerHTML = prompts[currentIndex];
+    currentIndex++
+
+    if(currentIndex == prompts.length){
+        currentIndex = 0;
+        this.prompts = this.shuffle(prompts)
     }
-
-
-
-    tweetDiv.append(tweetImage)
-    tweetDiv.append(tweetText)
-    container.append(tweetDiv)
 }
+
+function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+  
+    while (currentIndex != 0) {
+  
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+    console.log(array)
+    return array;
+  }
